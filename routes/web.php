8f1fd72bbde1ad976344a5da2096ccfebbcb7ecd
ViniciusSchiebelbein <stnicio@hotmail.com>
2021-03-12 17:37:13 -0300

    Rota para formulario e cadastro adicionadas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContatoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ContatoController::class, 'create']);
+Route::post('/contato', [ContatoController::class, 'store']);
